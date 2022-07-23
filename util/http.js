16bcb6aed8969ac31f99cfd10da7e524dec41199
Ayush <ayushagarwal.dev@gmail.com>
2022-07-23 16:00:24 +0530

    add fetch expense function

--- util/http.js
+++ util/http.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
+const BACKEND_URL =
+  "https://react-native-course-faff2-default-rtdb.firebaseio.com";
+
 export function storeExpense(expenseData) {
-  axios.post(
-    "https://react-native-course-faff2-default-rtdb.firebaseio.com/expenses.json",
-    expenseData
-  );
+  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+}
+export async function fetchExpenses() {
+  const res = axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in res.data) {
+    const expenseObj = {
+      id: key,
+      amount: res.data[key].amount,
+      date: new Date(res.data[key].date),
+      description: res.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
