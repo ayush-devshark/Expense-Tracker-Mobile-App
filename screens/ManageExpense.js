7f01becf5f7544a6af2280798717e666736eee55
Ayush <ayushagarwal.dev@gmail.com>
2022-07-23 16:45:54 +0530

    link firebase id to add expense state and transform received array

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -32,11 +32,11 @@ export default function ManageExpense({ route, navigation }) {
     navigation.goBack();
   }
 
-  function confirmHandler(expenseData) {
+  async function confirmHandler(expenseData) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      storeExpense(expenseData);
+      const id = await storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
 
--- util/http.js
+++ util/http.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const BACKEND_URL =
   "https://react-native-course-faff2-default-rtdb.firebaseio.com";
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+export async function storeExpense(expenseData) {
+  const res = await axios.post(BACKEND_URL + "/expenses.json", expenseData);
+  const id = res.data.name;
+  return id;
 }
 export async function fetchExpenses() {
   const res = await axios.get(BACKEND_URL + "/expenses.json");
